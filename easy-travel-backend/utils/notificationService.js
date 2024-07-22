498c2cf8806b592c26f1d8d9e04cfea52f6c56af
Derrick Alphaeus <dealphaeus@gmail.com>
2024-07-22 11:54:25 +0300

    Delete easy-travel-backend directory

--- easy-travel-backend/utils/notificationService.js
+++
@@ -1,7 +0,0 @@
-const client = require('../config/redis');
-
-const sendNotification = (userId, message) => {
-  client.publish('notifications', JSON.stringify({ userId, message }));
-};
-
-module.exports = sendNotification;
